957e978cdd66232a76092b128bebda07af507587
Anton <Ssniperok@gmail.com>
2016-04-19 15:30:02 +0300

    added buttons

--- src/main/java/views/MainFrame.java
+++ src/main/java/views/MainFrame.java
@@ -11,20 +11,28 @@ public class MainFrame extends JFrame{
 
     public static void main(String[] args) {
         JFrame mainFrame = new JFrame("Search My Files");
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 3));
+
+        JTextField searchInput = new JTextField(1);
+        panel.add(searchInput);
+
+        JButton search = new JButton("SEARCH");
+        search.setVerticalTextPosition(AbstractButton.CENTER);
+        panel.add(search);
+
+        JButton folder = new JButton("FOLDER");
+        folder.setVerticalTextPosition(AbstractButton.CENTER);
+        panel.add(folder);
+
+        mainFrame.add(panel);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         mainFrame.setPreferredSize(new Dimension(550, 450));
-        Inputs input = new Inputs();
         mainFrame.pack();
         mainFrame.setVisible(true);
     }
 
-    static class Inputs extends JPanel {
 
-        void fileNameInput() {
-            JTextField fileName = new JTextField();
-            fileName.setEditable(true);
-            add(fileName);
-        }
-    }
 }
